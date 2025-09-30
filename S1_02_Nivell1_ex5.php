e554c8867106fb3d9bd175071344f58e3dfef025
Victor C.B <victor.charneco.barba@gmail.com>
2025-09-30 23:00:57 +0200

    ex5 done

--- S1_02_Nivell1_ex5.php
+++ S1_02_Nivell1_ex5.php
@@ -1,15 +1,25 @@
 <?php
 
-function verificarGrau(¢nota){
-    if($nota >= 60){
+function verificarGrau($nota){
+    if($nota < 0 || $nota > 100){
+        return "ERROR: La nota ha d'estar entre 0 i 100";
+    }elseif($nota >= 60){
         return "Primera Divisió";
     }elseif($nota >= 45){
         return "Segona Divisió";
     }elseif($nota >= 33){
         return "Tercera Divisió";
     }else{
-        ERROR:
+        return "L'estudiant reprovarà";
     }
 }
+
+echo verificarGrau(70);
+echo "\n";
+echo verificarGrau(50);
+echo "\n";
+echo verificarGrau(40);
+echo "\n";
+echo verificarGrau(20);
 
 ?>
