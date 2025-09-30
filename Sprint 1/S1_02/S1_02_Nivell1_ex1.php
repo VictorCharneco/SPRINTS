62e57db55017e7392ab6efcc9546790a216afac2
Victor C.B <victor.charneco.barba@gmail.com>
2025-09-30 12:16:17 +0200

    Updated folder for s1_02

---
+++ Sprint 1/S1_02/S1_02_Nivell1_ex1.php
@@ -0,0 +1,13 @@
+<?php
+
+$nom_client = "Ruben";
+$anys_client = 38;
+$altura_client = 1.71;
+const ANY_NAIXEMENT = 1987;
+$es_professor = true;
+
+const NOM_MEU = "Victor";
+
+echo "Em dic " . NOM_MEU . ". Estudio PHP i el mentor es diu " . $nom_client . " i és professor (" . $es_professor . "). Va néixer al " . ANY_NAIXEMENT . ", per tant té "
+. $anys_client . " anys, i fa " . $altura_client . " cm."
+?>
---
+++ Sprint 1/S1_02/S1_02_Nivell1_ex2.php
@@ -0,0 +1,6 @@
+<?php
+$salutacio = "Hello world";
+
+
+
+?>
