66d0a89d3729fe8ce9db34970ded4a916818c600
Victor C.B <victor.charneco.barba@gmail.com>
2025-09-30 13:14:31 +0200

    Exercise 3(A) done

---
+++ S1_02_Nivell1_ex3.php
@@ -0,0 +1,28 @@
+<?php
+
+// (A)
+$var_X = 2;
+$var_Y = 4;
+$var_N = 9;
+$var_M = 3;
+
+echo "Valor de X és: " . $var_X . "\n" . "Valor de Y és: " . $var_Y . "\n" . 
+"La suma de X i Y és: " . ($var_X + $var_Y) . "\n" . "La resta de X i Y és: " . ($var_X - $var_Y) . "\n" .
+"El producte de X i Y és: " . ($var_X * $var_Y) . "\n" . "El mòdul de X i Y és: " . ($var_X / $var_Y) . "\n";
+
+echo "---------------------------- \n";
+
+echo "Valor de N és: " . $var_N . "\n" . "Valor de M és: " . $var_M . "\n" . "La suma de N i M és: " . ($var_N + $var_M) . "\n" . 
+"La resta de N i M és: " . ($var_N - $var_M) . "\n" . "El producte de N i M és: " . ($var_N * $var_M) . "\n" . 
+"El mòdul de N i M és: " . ($var_N / $var_M) . "\n";
+
+echo "---------------------------- \n";
+
+// (B)
+// Crea un programa que tingui una funció que rebi dos nombres per paràmetre, i en un tercer paràmetre que indiqui 
+// una operació aritmética(suma,resta,multiplicació o divisió dels dos nombres).
+// Pensa en tots els casos possibles que ha de tenir aquest programa abans de fer-lo!
+
+
+
+?>
