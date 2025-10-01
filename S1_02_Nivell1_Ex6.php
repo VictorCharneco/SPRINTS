2367e37faedda214a0684303f87547c271163980
Victor C.B <victor.charneco.barba@gmail.com>
2025-10-01 10:52:29 +0200

    ex6 done

--- S1_02_Nivell1_Ex6.php
+++ S1_02_Nivell1_Ex6.php
@@ -1,7 +1,16 @@
 <?php
 
-function mossegada(){
-    
+function isBitten(){
+    $numRandom = rand(0,1);
+    return $numRandom === 1;
 }
+
+if(isBitten()){
+    echo "T'ha mossegat el dit!!!";
+}else{
+    echo "Uiii! Encara tens dit!";
+}
+
+
 
 ?>
