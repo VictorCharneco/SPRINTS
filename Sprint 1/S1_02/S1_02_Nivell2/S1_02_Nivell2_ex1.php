7242ca9c7efe63110a8d887e580082dc533305d6
Victor C.B <victor.charneco.barba@gmail.com>
2025-10-01 13:41:27 +0200

    S1_02_Nivell2_ex1 done

--- Sprint 1/S1_02/S1_02_Nivell2/S1_02_Nivell2_ex1.php
+++ Sprint 1/S1_02/S1_02_Nivell2/S1_02_Nivell2_ex1.php
@@ -4,9 +4,10 @@
 // Cada minut adicional a partir dels 3 primers és un pas de compatador i costa 5 cntms
 
 function costTrucada($duracio){
-$calcul = $duracio * 0.5;
+$minGratis = 3;
+$calcul = ($duracio - $minGratis) * 0.5;
 
-    if($duracio < 0){
+    if($duracio <= 0){
         return "ERROR. Indica els minuts de la trucada.";
     }elseif($duracio > 3){
         return "El cost de la trucada és: " . $calcul;
@@ -14,5 +15,9 @@ $calcul = $duracio * 0.5;
         return "La teva trucada no té cost adicional.";
     }
 }
+
+echo costTrucada(5) . "\n";
+echo costTrucada(3) . "\n";
+echo costTrucada(0) . "\n";
 
 ?>
