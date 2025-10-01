10ff082f65dcbca9c84958679ad08973de47ac35
Victor C.B <victor.charneco.barba@gmail.com>
2025-10-01 13:57:25 +0200

    S1_02_Nivell2_ex1 updated applying a const var

--- Sprint 1/S1_02/S1_02_Nivell2/S1_02_Nivell2_ex1.php
+++ Sprint 1/S1_02/S1_02_Nivell2/S1_02_Nivell2_ex1.php
@@ -1,19 +1,20 @@
 <?php
 
+const MIN_GRATIS = 3;
+
 function costTrucada($duracio){
-$minGratis = 3;
-$calcul = ($duracio - $minGratis) * 0.5;
+$calcul = ($duracio - MIN_GRATIS) * 0.5;
 
     if($duracio <= 0){
-        return "ERROR. Indica els minuts de la trucada.";
+        return "/!\ ERROR. Indica els minuts de la trucada.";
     }elseif($duracio > 3){
-        return "El cost de la trucada és: " . $calcul;
+        return "El cost de la trucada és: " . $calcul . " euros.";
     }else{
         return "La teva trucada no té cost adicional.";
     }
 }
 
-echo costTrucada(5) . "\n";
+echo costTrucada(16) . "\n";
 echo costTrucada(3) . "\n";
 echo costTrucada(0) . "\n";
 
